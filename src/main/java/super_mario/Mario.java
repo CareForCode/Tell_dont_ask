cd89cf92d93e2f055e382a544d66319f08a7a1c7
Patrick Scheibert <patrick.scheibert@hotmail.com>
2022-02-23 08:26:37 +0100

    init commit

---
+++ src/main/java/super_mario/Mario.java
@@ -0,0 +1,15 @@
+package super_mario;
+
+public class Mario {
+    public Status getStatus() {
+        return null;
+    }
+
+    public void setStatus(Status status) {
+
+    }
+
+    public int getLifeCount() {
+        return 0;
+    }
+}
