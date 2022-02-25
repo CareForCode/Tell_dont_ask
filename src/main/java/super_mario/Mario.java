f16fb5edd30e807083d2d8f25c9958717aac970e
Patrick Scheibert <patrick.scheibert@outlook.com>
2022-02-25 10:02:36 +0100

    remove unnecessary constructor

--- src/main/java/super_mario/Mario.java
+++ src/main/java/super_mario/Mario.java
@@ -4,11 +4,6 @@ public class Mario {
     private State state;
     private int lifeCount;
 
-    public Mario() {
-        state = State.BIG;
-        lifeCount = 3;
-    }
-
     public Mario(State state, int lifeCount) {
         this.state = state;
         this.lifeCount = lifeCount;
